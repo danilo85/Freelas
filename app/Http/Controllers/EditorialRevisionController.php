b7314f9659cd7b818fce34de378a8751d5091450
Danilomiguel85 <danilo.a.miguel@hotmail.com>
2026-07-30 10:29:49 -0300

    Corrigir digitacao acidental me use em EditorialRevisionController.php

--- app/Http/Controllers/EditorialRevisionController.php
+++ app/Http/Controllers/EditorialRevisionController.php
@@ -38,7 +38,7 @@ class EditorialRevisionController extends Controller
 
         if ($request->filled('search')) {
             $search = $request->search;
-            $query->where(function ($q) me use ($search) {
+            $query->where(function ($q) use ($search) {
                 $q->where('title', 'like', "%{$search}%")
                   ->orWhere('description', 'like', "%{$search}%");
             });
